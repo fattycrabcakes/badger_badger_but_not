b6509b9528d07c6e9b213305bb1c6987c8401145
Fatty Crabcakes <johngaltatemyballs@gmail.com>
2017-06-29 16:10:14 -0700

    massive refactoring because that's how I roll bitches

--- gen_badger.pl
+++ gen_badger.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/env perl
 
 use strict;
+use lib("./lib");
 use feature qw(say);
 use YAML;
+use Badger;
 use Image::Magick;
 use FFmpeg::Command;
 use Data::Dumper;
@@ -12,245 +14,137 @@ use Term::ProgressBar;
 
 
 my %opt;
+GetOptions(\%opt,"config=s","out=s",'overwrite','tmpdir=s');
+die "Usage: $0 --config <file> --out <file> [--overwrite]" if (!-f $opt{config} || !exists $opt{out});
 
-GetOptions(\%opt,"config=s","out=s","gif","scale=s",'report','overwrite');
-die "Usage: $0 --config <file> --out <file> [--gif] [--overwrite] [--scale <float>]" if (!-f $opt{config} || !exists $opt{out});
+$opt{tmpdir}||="/tmp";
 
-my $config = YAML::LoadFile($opt{config});
-my @timeline = @{$config->{timeline}};
+our $badger = Badger->new(config=>$opt{config});
+$badger->consume(%opt);
 
-foreach my $asset (keys %{$config->{assets}}) {
-	die "$config->{assets}->{$asset} not found" if (!-f $config->{assets}->{$asset});
+
+foreach my $asset (keys %{$badger->conf("assets")}) {
+	die "file fo $asset not found" if (!-f $badger->asset($asset)->{src});
 }
 
-if (-f $opt{out} && !$opt{overwrite} && !$opt{report}) {
+if (-f $opt{out} && !$opt{overwrite}) {
 	print STDERR "$opt{out} already exists. Overwrite? (Y/N) ";
-	ReadMode 1;
 	my $ow = lc(ReadKey(0));
-	ReadMode(1);
-	if($ow ne "y") {
+	if(lc($ow) ne "y") {
 		exit;
 	}
 }
 
-my $background = Image::Magick->new();
-my $sprite = Image::Magick->new();
-my $mushroom = Image::Magick->new();
-
-my $has_gifsicle = length(qx(which gifsicle));
-if (!$has_gifsicle && $opt{gif}) {
-	say STDERR "\n\nYou should probably install gifsicle for optimizing the huge GIF that Imagemagick produces.";
-	say STDERR "Seriously. They're giant.\n\n";
-}
-
-$background->Read(filename=>$config->{assets}->{background});
-$sprite->Read($config->{assets}->{sprite});
-$mushroom->Read($config->{assets}->{mushroom});
-
-$sprite = split_frames(img=>$sprite);
-$mushroom = split_frames(img=>$mushroom);
-
-my ($sprites,$mushrooms,$timeline_length) = (scalar(@$sprite),scalar(@$mushroom),scalar(@timeline));
+my ($background,$sprites,$mushrooms,$snakes) = map {$badger->load_img(img=>$badger->asset($_)->{src})} qw(background badger mushroom snake);
 my ($bgw,$bgh) = $background->Get("width","height");
-my ($sprite_width,$sprite_height) = $sprite->[0]->Get("width","height");
-
-my $sprite_scaled = {'1'=>$sprite};
-my $scp = Term::ProgressBar->new({term_width=>80,name=>"Scaling Sprites",count=>$timeline_length,remove=>0});
-foreach my $spr (@timeline[1..$timeline_length]) {
-	if (!defined $sprite_scaled->{$$spr[2]}) {
-		$sprite_scaled->{$$spr[2]} = [
-			map {$_->Resize(width=>$sprite_width*$$spr[2],height=>$sprite_height*$$spr[2]);$_} @{$sprite->Clone()}
-		];
-	}
-}
-
-#my $total_frames = $config->{badger_every}*$timeline_length;
-
-my $bd = 4.5;
-my $total_frames = $config->{fps}*$bd;
-$config->{badger_every} = int($total_frames/$timeline_length);
-#$total_frames += ($total_frames % $sprites);
-
-say STDERR sprintf("\nGenerating %d frames (%d seconds)",$total_frames,$total_frames/$config->{fps});
-if ($opt{report}) {
-	exit;
-}
-
-my $output = Image::Magick->new();
-my @cleanup = ();
-my $badgers_in_play = [];
 
 
-my $frame_pattern = "/tmp/frame-%04d.png";
-my $current_frame=0;
-my $frame_delay = int((1/$config->{fps})*100);
-my $fps_diff = $config->{badgerfps}/$config->{fps};
-my $bp = Term::ProgressBar->new({term_width=>80,name=>"Rendering Badgers",count=>$total_frames-1,remove=>0});
+my $rendered = {
+	badgers=>render_badgers(sprites=>$sprites,bg=>$background),
+	mushrooms=>render_mushrooms(sprites=>$mushrooms,bg=>$background),
+	snake=>render_snake(sprites=>$snakes,bg=>$background)
+};
+$badger->render_final(segments=>$rendered);
 
 
-for (my $i=0;$i<$total_frames;$i++) {
-	# new badger every half second
 
-	if ($i % $config->{badger_every}==0) {
-		if ($#timeline) {
-			push(@$badgers_in_play,shift(@timeline));
-		}
-	}
-
-	my $frame_to_render = int(($i*$fps_diff) % $sprites);
-	my $frame = render_frame($background,$sprite_scaled,$badgers_in_play,$frame_to_render);
-
-	#ImageMagick dows _not_ like it when stuff sticks over the edge
-	$frame->Crop(width=>$bgw,height=>$bgh,x=>0,y=>0);
-
-	if (!$opt{gif}) {
-		my $opf = sprintf($frame_pattern,++$current_frame);
-		$frame->Write($opf);
-		push(@cleanup,$opf);
-		#undef $frame;
-	} else {
-		$frame->Set(delay=>$frame_delay);
-		$frame->Set(dispose=>"Background");
-		push(@$output,$frame);
-	} 
-	$bp->update($i);
-}
-
-my $mushroom_duration = 1.5;
-
-my $mtr = int($config->{fps}*$mushroom_duration);# $mushrooms**($mushroom_duration);
-my $frame_delay = 1/($mtr);
-
-say "Rendering $mtr mushroom frames";
-
-my $mp = Term::ProgressBar->new({term_width=>80,name=>"Rendering Mushrooms",count=>$mtr-1,remove=>0});
-my $fps_diff = 16/$mtr;
-
-for (my $i=0;$i<$mtr;$i++) {
-	 my $frame_to_render = int(($i*$fps_diff) % $mushrooms);
-	if (!$opt{gif}) {
-		my $opf = sprintf($frame_pattern,++$current_frame);
-		$mushroom->[$frame_to_render]->Write($opf);
-		push(@cleanup,$opf);
-	} else {
-		my $delay = 1/($mtr*2);
-		my $frame = $background->Clone();
-
-		my $m = $mushroom->[$i % $mushrooms]->Clone();
-		$frame->Set(delay=>$delay);
-		$frame->Set(dispose=>"Background");
-		$frame->Composite(image=>$m,compose=>"Over",gravity=>"NorthWest");
-
-		#$frame->Quantize(colors=>256,colorspace=>"RGB");
-		undef $m;
-		push(@$output,$frame);
-	}
-	$mp->update();
-}
+sub render_badgers {
+	my (%args) = @_;
 
 
-if (!$opt{gif}) {
-	say STDERR "\nGenerating Video....";
-
-	my $ff = FFmpeg::Command->new();
-	$ff->input_file("/tmp/frame-%04d.png");
-	$ff->output_file($opt{out});
-
-	$ff->options(
-		'-framerate'=>$config->{fps},
-		'-crf' => 20,
-		'-profile:v'=>'high',
-		'-pix_fmt' => 'yuv420p',
-		'-an',
-		'-y',
-	);
-	$ff->exec();
+	my $sprites = $args{sprites};
+	my $conf = $badger->asset("badger");
+	my ($sprite_width,$sprite_height) = $sprites->[0]->Get("width","height");
 	
-	my $cp = Term::ProgressBar->new({term_width=>80,name=>"Cleaning Up",count=>$#cleanup-1,remove=>0});
-	my $cpc = 0;
-	foreach my $file (@cleanup) {
-		unlink($file);
-		$cp->update(++$cpc);
-	}
-	say "";
-} else {
-	if ($opt{scale}) {
-		my $sp = Term::ProgressBar->new({term_width=>80,name=>"Scaling",count=>scalar(@$output)-1,remove=>0});
-		my $spc=0;
-		foreach my $frame (@$output) {
-			$frame->Resize(width=>$bgw*$opt{scale},height=>$opt{scale}*$bgh);
-			$sp->update(++$spc);
-		}
-	}
-	say "";
-	say STDERR "Writing JIF";
-    $output->write($opt{out});
+	my $total_frames = $badger->conf("fps")*$conf->{duration};
 
-	if ($has_gifsicle) {
-		my $tmpfile = "$opt{out}.tmp";
+	my @timeline = @{$badger->conf("timeline")};
+	my $timeline_length = scalar(@timeline);
 
-		my $size = (stat($opt{out}))[7];
-		
-		say STDERR "Optimizing....";
-		rename($opt{out},$tmpfile);
-		system("gifsicle $tmpfile --colors=256  --optimize=3 -o $opt{out}");
-		my $reduction = $size-(stat($opt{out}))[7];
-		my $reduction_scale = "K";
-		if ($reduction>=2**20) {
-			$reduction_scale="M";
-			$reduction/=2**20;
-		} else {
-			$reduction/=2**10;
-		}
-		
-		say STDERR sprintf("Reduced file by %0.2f%s",$reduction,$reduction_scale);
-		unlink($tmpfile);
+	my $badger_every = int($total_frames/$timeline_length+1);
+	my $fps_diff = $conf->{fps}/$badger->conf("fps");
+
+	my $scale_progress = $badger->progress(name=>"Scaling Sprites",count=>$timeline_length-1);
+	my $scale_i=0;
+
+	my $sprite_scaled = {
+		'1'=>$sprites,
+	};
+	foreach my $spr (@timeline) {
+    	if (!defined $sprite_scaled->{$$spr[2]}) {
+        	$sprite_scaled->{$$spr[2]} = [
+            	map {$_->Resize(width=>$sprite_width*$$spr[2],height=>$sprite_height*$$spr[2]);$_} @{$sprites->Clone()},
+			];
+    	}
+		$scale_progress->update(++$scale_i);
 	}
+
+	my $progress = $badger->progress(name=>"Rendering Badgers",count=>$total_frames-1);
+	my @badgers_visible = ();
+	my @files;
+
+	for (my $i=0;$i<$total_frames;$i++) {
+    	if ($i % $badger_every==0) {
+        	if (scalar(@timeline)) {
+            	push(@badgers_visible,shift(@timeline));
+        	}
+    	}
+    	my $frame_to_render = int(($i*$fps_diff) % scalar(@$sprites));
+
+		my $frame = $args{bg}->Clone();
+    	foreach my $badger (reverse @badgers_visible) {
+        	my $st = $sprite_scaled->{$badger->[2]}->[$frame_to_render];
+        	$frame->Composite(image=>$st,gravity=>"NorthWest",x=>$badger->[0],y=>$badger->[1]);
+    	}
+		$badger->writefile($frame,$i);
+    	$progress->update($i);
+	}
+	my $ret = $badger->render_video(sequence=>"badger");
 }
 
-sub render_frame {
-	my ($bg,$sprite,$all,$current_frame) = @_;
+sub render_mushrooms {
+    my (%args) = @_;
 
-	my $nbg = $bg->Clone();
-	foreach my $badger (reverse @$all) {
-		my $st = $sprite->{$badger->[2]}->[$current_frame];
-		$nbg->Composite(image=>$st,compose=>"Over",gravity=>"NorthWest",x=>$badger->[0],y=>$badger->[1]);
-		undef $st;
-		
+	my $conf = $badger->asset("mushroom");
+    my $total_frames = int($badger->conf("fps")*$conf->{duration});
+    my $fps_diff = $conf->{fps}/$total_frames;
+	my $sprites = $args{sprites};
+    my $progress = $badger->progress(name=>"Rendering Mushrooms",count=>$total_frames-1);
+	
+    for (my $i=0;$i<$total_frames;$i++) {
+        my $frame_to_render = int(($i*$fps_diff) % scalar(@$sprites));
+		$badger->writefile($sprites->[$frame_to_render],$i);
+    	$progress->update($i);
 	}
-	return $nbg;
+	return $badger->render_video(sequence=>"mushroom");
 }
 
-sub split_frames {
-    my (%opt) = @_;
+sub render_snake {
+	my (%args) = @_;
 
-    my $height = $opt{img}->Get("height");
-    my $width = $opt{width};
-    my $frames =0;
+	my $sprites = $args{sprites};
+	my $conf = $badger->asset("snake");
+	my $total_frames = $badger->conf("fps")*$conf->{"duration"};
+    my $fps_diff = $conf->{fps}/$badger->conf("fps");
+	my $x= -$sprites->[0]->Get("width");
+	my $inc = ($args{bg}->Get("Width")+$sprites->[0]->Get("width"))/$total_frames;
 
-    if (!$width) {
-        my @pixels = $opt{img}->GetPixels(width=>int($opt{img}->Get("width")/2)+16,height=>1,x=>0,y=>$height/2,map=>"RGBA");
-        $width=0;
-        for (my $i=0;$i<scalar(@pixels);$i+=4) {
-            my $pixel = (($pixels[$i]&0xff)<<24)|(($pixels[$i+1]&0xff)<<16)|(($pixels[$i+2]&0xff<<8))|($pixels[$i+3]&0xff);
-            if ($pixel==0xB0FADEE5) {
-                $width=int($i/4);
-                last;
-            }
-        }
-        if ($width==0) {
-            die "Unable to find registration mark 0xB0FADEE5 on sprite $opt{file}";
-        }
-    }
+	my $progress = $badger->progress(name=>"Rendering snake",count=>scalar($total_frames)-1);
 
-    my $frames = int($opt{img}->Get("width")/$width);
-    my $ret = Image::Magick->new();
-    for (my $i=0;$i<$frames;$i++) {
-        my $frame = $opt{img}->Clone();
-        $frame->Crop(width=>$width,height=>$height,x=>($i*$width)+$i,y=>0);
-        push(@$ret,$frame);
-    }
-    return $ret;
+
+	for (my $i=0;$i<$total_frames;$i++) {
+    	my $frame_to_render = int(($i*$fps_diff) % scalar(@$sprites));
+
+    	my $frame = $args{bg}->Clone();
+    	my $st = $sprites->[$frame_to_render];
+    	$frame->Composite(
+			image=>$st,compose=>"Over",gravity=>"NorthWest",
+			x=>$x,y=>$conf->{y},
+		);
+		$badger->writefile($frame,$i);
+    	$x+=$inc;
+    	$progress->update($i);
+	}
+	return $badger->render_video(sequence=>"snake");
 }
-
 
---
+++ lib/Badger.pm
@@ -0,0 +1,175 @@
+package Badger;
+
+use strict;
+use Modern::Perl;
+use Image::Magick;
+use FFmpeg::Command;
+use Term::ProgressBar;
+use YAML;
+use Moo;
+
+has config=>(is=>'ro');
+has configuration=>(is=>'lazy');
+has filemask=>(is=>'lazy');
+
+has tempfiles=>(is=>'rw',default=>sub {[];});
+has format =>(is=>'rw',default => sub {"mp4";});
+
+
+
+sub progress {
+	my ($self,%args) = @_;
+
+	return Term::ProgressBar->new({term_width=>80,name=>$args{name},count=>$args{count},remove=>0});
+}
+
+sub render_video {
+    my ($self,%args) = @_;
+
+    say STDERR "\nRendering video for $args{sequence}";
+    my $output_filename = sprintf("%s/%s.%s",$self->conf("tmpdir"),$args{sequence},$self->format);
+
+    my $ff = FFmpeg::Command->new();
+    $ff->input_file($self->filemask);
+    $ff->output_file($output_filename);
+    $ff->options(
+        '-framerate'=>$self->conf("fps"),
+        '-crf' => 20,
+        '-profile:v'=>'high',
+        '-pix_fmt' => 'yuv420p',
+        '-an',
+        '-y',
+    );
+    $ff->exec();
+
+	my @cleanup = @{$self->tempfiles};
+
+    my $cp = $self->progress(name=>"Cleaning Up",count=>scalar(@{$self->tempfiles}));
+    my $cpc = 0;
+    foreach my $file (@cleanup) {
+        unlink($file);
+        $cp->update(++$cpc);
+    }
+    say "";
+	$self->tempfiles([]);
+    return $output_filename;
+}
+
+
+sub render_final {
+    my ($self,%args) = @_;
+
+    say STDERR "Rendering $args{config}->{out}";
+
+    my $cfile = sprintf("%s/concat_files.txt",$self->conf("tmpdir"));
+
+    open(FO,">",$cfile);
+    foreach my $scene (@{$self->conf("scenes")}) {
+        say FO sprintf("file '%s'",concat_filename($args{segments}->{$scene})) if (-f $args{segments}->{$scene});
+    }
+    close(FO);
+
+    # apparently FFMpeg::COmmand goofs up the order of args
+    my $res = `ffmpeg -f concat -safe 0 -y -i $cfile -c copy $self->{configuration}->{out}`;
+
+    unlink($cfile);
+    unless($self->conf("preserve_tempfiles")) {
+        foreach my $file (values %{$args{segments}}) {
+            unlink($file);
+        }
+    }
+}
+
+
+sub load_img {
+    my ($self,%opt) = @_;
+
+    my $img = Image::Magick->new();
+    $img->Read($opt{img});
+
+    my $height = $img->Get("height");
+    my $width = 0;
+
+    if (!$width) {
+        my @pixels = $img->GetPixels(width=>int($img->Get("width")/2)+16,height=>1,x=>0,y=>$height/2,map=>"RGBA");
+        $width=0;
+        for (my $i=0;$i<scalar(@pixels);$i+=4) {
+            my $pixel = (($pixels[$i]&0xff)<<24)|(($pixels[$i+1]&0xff)<<16)|(($pixels[$i+2]&0xff<<8))|($pixels[$i+3]&0xff);
+            if ($pixel==0xB0FADEE5) {
+                $width=int($i/4);
+                last;
+            }
+        }
+        if ($width==0) {
+            return $img;
+        }
+    }
+
+    my $frames = int($img->Get("width")/$width);
+    my $ret = Image::Magick->new();
+    for (my $i=0;$i<$frames;$i++) {
+        my $frame = $img->Clone();
+        $frame->Crop(width=>$width,height=>$height,x=>($i*$width)+$i,y=>0);
+        push(@$ret,$frame);
+    }
+    return $ret;
+}
+
+sub writefile {
+	my ($self,$img,$index) = @_;
+
+	my $filename = sprintf($self->filemask,$index);
+    $img->Write($filename);
+	$self->tempfile($filename);
+	undef $img;
+}
+
+sub tempfile {
+	my ($self,$path) = @_;
+
+	push (@{$self->tempfiles},$path);
+}
+
+sub concat_filename {
+    my ($file) = @_;
+
+    unless ($file=~/^\//) {
+        $file= substr($file,rindex($file,"/")+1);
+    }
+    return $file;
+}
+
+sub conf {
+	my ($self,$key) = @_;
+
+	return $self->configuration->{$key};
+}
+
+sub asset {
+	my ($self,$name) = @_;
+
+	return $self->conf("assets")->{$name};
+}
+
+sub consume {
+	my ($self,%args) = @_;
+
+	my $conf = $self->configuration;
+	foreach my $k (keys %args) { $conf->{$k} = $args{$k}; }
+}
+
+
+sub _build_configuration {
+	my ($self) = shift;
+	my $conf = YAML::LoadFile($self->config);
+}
+
+sub _build_filemask {
+	my ($self) = shift;
+
+	return join("/",$self->conf("tmpdir"),"frame-%02d.png");
+}
+
+
+no Moo;
+1;
